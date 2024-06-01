a4eac73010baff5bd9ec60147f610b51cda8f097
Nicholas <gianfrancopr08@gmail.com>
2024-06-01 18:03:46 -0500

    implementation of operations

--- modelo.py
+++ modelo.py
@@ -1,3 +1,25 @@
-print('Hello world')
-print('Nicooooo')
-print('Hola mundo')
+def sumar(a,b):
+    return a+b
+
+def restar(a,b):
+    return a-b
+
+def multiplicar(a,b):
+    return a*b
+
+def dividir(a,b):
+    return a/b
+
+def potencia(a,b):
+    return a**b
+
+def raiz(a,b):
+    return a**(1/b)
+
+def factorial(a):
+    if a == 0:
+        return 1
+    else:
+        return a*factorial(a-1)
+    
+print(sumar(5,3))
